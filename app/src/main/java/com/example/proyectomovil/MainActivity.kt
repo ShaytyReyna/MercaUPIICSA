7abb028f31e29b198e5a32b5d8546e3494bede21
ShaytyReyna <shayty.reyna@gmail.com>
2024-04-26 21:32:17 -0600

    CambioMain
    Cambie el main para que no tenga lo predeterminado de la plantilla, y puse el logo de upiicsa asi como un textView

--- app/src/main/java/com/example/proyectomovil/MainActivity.kt
+++ app/src/main/java/com/example/proyectomovil/MainActivity.kt
@@ -18,7 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
+        /*
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -31,9 +32,10 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }*/
     }
 
+/*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -54,5 +56,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
-    }
+    }*/
 }
